9539def5ed8fdbf01d5f54a9361bbb1c879840fa
Romic <romic.huang@qq.com>
2017-08-03 02:42:55 +0000

    Delete all snippets. Ref#12

--- backend/models.py
+++ backend/models.py
@@ -6,10 +6,6 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
-
-class Snippet(models.Model):
-    name = models.CharField(max_length=100, blank=True, default='')
-
 class Admin(models.Model):
     email=models.EmailField(max_length=200,default='')
     nickname=models.CharField(max_length=100,blank=True, default='')
