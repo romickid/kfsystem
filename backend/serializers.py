051a91a9d92e1dc29e477eda66910a6f4b8750ac
Bamboo <1519546595@qq.com>
2017-08-04 02:46:25 +0000

    Modify serializer model. Ref#17

--- backend/serializers.py
+++ backend/serializers.py
@@ -34,22 +34,7 @@ class CustomerServiceSerializer(serializers.Serializer):
         return instance
 
 
-class ChattingLogSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    client_id=serializers.CharField(max_length=100,default='null')
-    service_id=serializers.CharField()
-    content=serializers.CharField(max_length=500,default='null')
-    is_client=serializers.BooleanField(default=False)
-    time=serializers.DateTimeField()
-
-    def create(self, validated_data):
-        return ChattingLog.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.client_id = validated_data.get('client_id', instance.client_id)
-        instance.service_id = validated_data.get('service_id', instance.service_id)
-        instance.content = validated_data.get('content', instance.content)
-        instance.is_client = validated_data.get('is_client', instance.is_client)
-        instance.time = validated_data.get('time', instance.time)
-        instance.save()
-        return instance
+class ChattingLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =ChattingLog
+        fields=('id','client_id','service_id','content','is_client','time')
--- frontend/src/components/Hello.vue
+++ frontend/src/components/Hello.vue
@@ -38,7 +38,7 @@
   
     <div>
       <div>hello</div>
-      <div>{{item}}</div>
+      <div>{{test}}</div>
       <button @click="getData()">getData</button>
       <button @click="postData()">postData</button>
       <button @click="deleteData()">deleteData</button>
@@ -58,9 +58,9 @@ export default {
       api_set_profile: './api/admin_set_profile/',
       api_login: './api/admin_login/',
       api_reset_password: './api/admin_reset_password/',
-      api_chattinglog_send_message:'./api/chattinglog_send_message/',
-      api_chattinglog_delete_record:'./api/chattinglog_delete_record/',
-      api_chattinglog_get_data:'./api/',
+      api_chattinglog_send_message: './api/chattinglog_send_message/',
+      api_chattinglog_delete_record: './api/chattinglog_delete_record/',
+      api_chattinglog_get_data: './api/',
       item: {},
       gridData: '',
       test: {}
@@ -74,19 +74,14 @@ export default {
       vm.$http.get('./api/1/')
         .then((response) => {
           vm.$set(this, 'test', response.data)
-          //this.item = { client_id:'hhh', service_id: response.data['id'], content:'qwer', is_client:true, time:'2013-07-10 12:23:42'}
-          console.log(this.item)
         })
     },
 
     postData: function () {
       var vm = this
-      //vm.getData()
-      //console.log(this.test)
-      this.item = { client_id:'hhh', service_id:'12', content:'qwer', is_client:true, time:'2013-07-10 12:23:42'} // for test, do not delete it.
+      this.item = { client_id: 'hhh', service_id: 1, content: 'qwer', is_client: true, time: '2013-07-10 12:23:42'} // for test, do not delete it.
       vm.$http.post(vm.api_chattinglog_send_message, this.item)
-        .then((response) => {
-          //vm.$set(this, 'item', {})
+        .then((response) => {          
         })
     },
 
