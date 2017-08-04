7cb58265564c61f74edf789ace87c8ce8a40df49
Archangel <xiaochen.gao@icloud.com>
2017-08-04 09:00:57 +0000

    Decorate mainpage. Ref#35

--- frontend/src/pages/mainpage/mainpage.js
+++ frontend/src/pages/mainpage/mainpage.js
@@ -3,10 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import VueResource from 'vue-resource'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
 Vue.use(VueResource)
+Vue.use(iView)
 
 Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
