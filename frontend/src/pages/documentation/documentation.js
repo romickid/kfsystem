002765a1ffa2a03453a94d9e74994b10a18a337e
Archangel <xiaochen.gao@icloud.com>
2017-08-04 12:57:07 +0000

    Decorate document page. Ref#36

--- frontend/src/pages/documentation/documentation.js
+++ frontend/src/pages/documentation/documentation.js
@@ -2,8 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueResource from 'vue-resource'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+
+Vue.use(VueResource)
+Vue.use(iView)
 
 Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
