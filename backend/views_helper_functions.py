e0c4322b5c04e4cdfdf169303713a5185b0e3672
Romic <romic.huang@qq.com>
2017-08-08 13:42:07 +0000

    Add new helper funtions to achieve sending register email. Ref#85

--- backend/views_helper_functions.py
+++ backend/views_helper_functions.py
@@ -68,7 +68,7 @@ def admin_generate_vid(email):
     return admin_generate_communication_key(email)
 
 
-def admin_send_email(email, content):
+def admin_send_email_find_password(email, content):
     send_mail('客服系统找回密码', content, 'big5_nankai@163.com', [email], fail_silently=True)
 
 
@@ -138,5 +138,9 @@ def cs_generate_vid(email):
     return admin_generate_communication_key(email)
 
 
-def cs_send_email(email, content):
+def cs_send_email_create_account(email, content):
+    send_mail('客服系统创建账户', content, 'big5_nankai@163.com', [email], fail_silently=True)
+
+
+def cs_send_email_find_password(email, content):
     send_mail('客服系统找回密码', content, 'big5_nankai@163.com', [email], fail_silently=True)
