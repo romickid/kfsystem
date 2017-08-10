975000a62d0591778572c41b694470b866e567a3
Archangel <xiaochen.gao@icloud.com>
2017-08-10 03:57:02 +0000

    Modify customer service register. Ref #120

--- frontend/src/pages/se_folders/se_folders.js
+++ frontend/src/pages/se_folders/se_folders.js
@@ -5,11 +5,13 @@ import App from './App'
 import VueResource from 'vue-resource'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
+import utils from './utils'
 
 Vue.use(VueResource)
 Vue.use(iView)
 
 Vue.config.productionTip = false
+Vue.prototype.$utils = utils
 
 /* eslint-disable no-new */
 new Vue({
