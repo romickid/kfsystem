297c612647710fec431beac2f0384be3eb485214
魏久麒 <305347436@qq.com>
2017-08-22 09:13:47 +0000

    Add new files. Ref #274

---
+++ enterprise/frontend/src/components/Hello.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
+      </li>
+      <li>
+        <a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
+      </li>
+      <br>
+      <li>
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
+      </li>
+      <li>
+        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
+      </li>
+      <li>
+        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
+      </li>
+    </ul>
+  
+    <div>
+      <div>hello</div>      
+      <div>{{ gridData }}</div>
+      <button @click="getData()">getData</button>
+      <button @click="test1()">Test1</button>
+      <button @click="test2()">Test2</button>
+      <button @click="test3()">Test3</button>
+      <button @click="test4()">Test4</button>
+      <button @click="test5()">Test5</button>
+      <button @click="test8()">test_delete_ontime</button>
+      <button @click="test9()">test_status_change</button>
+      <div>{{ status }}</div>
+    </div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      // for test, do not delete it.
+
+      api_chattinglog_send_message: './api/chattinglog_send_message/',
+      api_chattinglog_delete_record: './api/chattinglog_delete_record/',
+      api_chattinglog_get_data: './api/chattinglog_get_data/',
+      api_chattinglog_delete_record_ontime: './api/chattinglog_delete_record_ontime/',
+      api_chattinglog_status_change: './api/chattinglog_status_change/',
+
+      api_create: './api/customerservice_create/',
+      api_set_profile: './api/customerservice_set_profile/',
+      api_login: './api/customerservice_login/',
+      api_reset_password: './api/customerservice_reset_password/',
+      api_validity: './api/serialnumber_validity/',
+      api_mark_used: './api/serialnumber_mark_used/',
+
+      item: {},
+      gridData: '',
+      test: {},
+      status: ''
+
+    }
+  },
+
+  // FOR TEST, DO NOT DELETE IT!
+  methods: {
+    test1: function () {
+      var vm = this
+      this.item = { 'email': 'test2@a.com' }
+      vm.$http.post(vm.api_create, this.item)
+        .then((response) => {
+          vm.$set(this, 'item', {})
+        })
+    },
+
+    test2: function () {
+      var vm = this
+      this.item = { 'email': 'test4@a.com' }
+      vm.$http.post(vm.api_create, this.item)
+        .then((response) => {
+          vm.$set(this, 'item', {})
+        })
+    },
+
+    test3: function () {
+      var vm = this
+      this.item = { 'email': 'test4@a.com', 'password': 'pass4' }
+      vm.$http.post(vm.api_set_profile, this.item)
+        .then((response) => {
+          vm.$set(this, 'item', {})
+        })
+    },
+
+    test4: function () {
+      var vm = this
+      this.item = { 'email': 'test4@a.com', 'nickname': 'nick4' }
+      vm.$http.post(vm.api_set_profile, this.item)
+        .then((response) => {
+          vm.$set(this, 'item', {})
+        })
+    },
+
+    test5: function () {
+      var vm = this
+      this.item = { 'email': 'test4@a.com', 'nickname': 'nick4', 'password': 'pass4' }
+      vm.$http.post(vm.api_set_profile, this.item)
+        .then((response) => {
+          vm.$set(this, 'item', {})
+        })
+    },
+
+    test6: function () {
+      var vm = this
+      this.item = { 'serials': 'abc124' }
+      vm.$http.post(vm.api_mark_used, this.item)
+        .then((response) => {
+          vm.$set(this, 'item', {})
+        })
+    },
+
+    test7: function () {
+      var vm = this
+      this.item = { 'serials': 'abc123' }
+      vm.$http.post(vm.api_mark_used, this.item)
+        .then((response) => {
+          vm.$set(this, 'item', {})
+        })
+    },
+
+    test8: function () {
+      var vm = this
+      // this.item = { client_id: 'hhh', service_id: 1, content: 'qwer', is_client: true, time: '2013-07-10 12:23:42' }
+      vm.$http.delete(vm.api_chattinglog_delete_record_ontime)
+        .then((response) => {
+          console.log('Complete delete on time!')
+          vm.$set(this, 'gridData', response.data)
+        })
+    },
+
+    test9: function () {
+      var vm = this
+      vm.$http.get(vm.api_chattinglog_status_change)
+        .then((response) => {
+          vm.$set(this, 'gridData', response.data)
+          console.log('Change status!')
+        })
+    },
+
+    getData: function () {
+      var vm = this
+      vm.$http.get(vm.api_chattinglog_get_data)
+        .then((response) => {
+          vm.$set(this, 'gridData', response.data)
+        })
+    }
+
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ enterprise/frontend/src/components/en_forget_password.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <Button type="text" @click="find = true">忘记密码</Button>
+    <Modal v-model="find" title="找回密码页" @on-ok="ok" @on-cancel="cancel">
+      <p>登录邮箱：</p>
+      <br>
+      <i-input v-model="email" id="input" @on-blur="checkEmail" @on-focus="emailInput"></i-input>
+      <i-label v-if="emailIllegal">
+        <p class="p">请输入正确的邮箱！</p>
+      </i-label>
+    </Modal>
+  </div>
+</template>
+<script>
+import 'iview/dist/styles/iview.css'
+export default {
+  data () {
+    return {
+      find: false,
+      email: '',
+      emailIllegal: false,
+      api_forget_password_email_request: '../api/admin_forget_password_email_request/',
+      item: {}
+    }
+  },
+  methods: {
+    checkEmail () {
+      let reg = /^[a-z0-9]([a-z0-9]*[-_]?[a-z0-9]+)*@([a-z0-9]*[-_]?[a-z0-9]+)+[\.][a-z]{2,3}([\.][a-z]{2})?$/i
+      let legal = reg.test(this.email)
+      if (legal === false && this.email !== '') {
+        this.emailIllegal = true
+      } else {
+        this.emailIllegal = false
+      }
+    },
+    emailInput () {
+      this.emailIllegal = false
+    },
+    ok () {
+      if (this.email === '' || this.emailIllegal === true) {
+        this.$Message.info('您的邮箱不正确！')
+      } else {
+        // 与后端链接进行信息传输和验证
+        this.item = {
+          'email': this.email
+        }
+        this.communicate()
+      }
+      this.cancel()
+    },
+    communicate () {
+      this.$http.post(this.api_forget_password_email_request, this.item)
+        .then((response) => {
+          if (response.data === 'ERROR, wrong email.') {
+            this.$Message.info('您的邮箱不正确！')
+          } else if (response.data === 'ERROR, invalid data in serializer.') {
+            window.location.href = '../notfound'
+          } else if (response.data === 'ERROR, incomplete information.') {
+            window.location.href = '../notfound'
+          } else if (response.data === 'ERROR, wrong information.') {
+            window.location.href = '../notfound'
+          } else {
+            this.$Message.info('验证邮件已发送至您的邮箱')
+          }
+        }, (response) => {
+          window.location.href = '../notfound'
+        })
+    },
+    cancel () {
+      this.email = ''
+      this.emailIllegal = false
+    }
+  }
+}
+</script>
+
+<style>
+.p {
+  color: red;
+}
+
+#input {
+  width: 300px;
+}
+</style>
