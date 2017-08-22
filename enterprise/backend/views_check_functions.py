0818fb634f237eed2accd0cd8afda1b35a42a175
romickid <romic.kid@gmail.com>
2017-08-22 18:28:39 +0000

    Rebuild backend helper functions. Ref #283

--- enterprise/backend/views_check_functions.py
+++ enterprise/backend/views_check_functions.py
@@ -1,20 +1,20 @@
 from .views_helper_functions import *
 
 
-def admin_create_check(json_receive):
-    test_json = json_testing(json_receive, ['email', 'nickname', 'password'], 3)
+def customer_create_check(json_receive):
+    test_json = json_testing(json_receive, ['email', 'nickname', 'password', 'telephone', 'location'], 5)
     if test_json == 1:
         return 0, 'ERROR, incomplete information.'
     if test_json == 2:
         return 0, 'ERROR, wrong information.'
-    if admin_is_existent_by_email(json_receive['email']) == True:
+    if customer_is_existent_by_email(json_receive['email']) == True:
         return 0, 'ERROR, email has been registered.'
-    if admin_is_existent_by_nickname(json_receive['nickname']) == True:
+    if customer_is_existent_by_nickname(json_receive['nickname']) == True:
         return 0, 'ERROR, nickname has been used.'
     return 1, 'No ERROR.'
 
 
-def admin_login_check(json_receive):
+def customer_login_check(json_receive):
     test_json = json_testing(json_receive, ['email', 'password'], 2)
     if test_json == 1:
         return 0, 'ERROR, incomplete information.'
@@ -23,118 +23,30 @@ def admin_login_check(json_receive):
     return 1, 'No ERROR.'
 
 
-def admin_reset_password_check(json_receive, request):
-    test_json = json_testing(json_receive, ['password', 'newpassword'], 2)
-    test_sessions = admin_sessions_check(request)
+def customer_show_user_info_check(request):
+    test_sessions = customer_sessions_check(request)
+    if test_sessions == False:
+        return 0, 'ERROR, session is broken.'
+    if customer_is_existent_by_email(request.session['ec_email']) == False:
+        return 0, 'ERROR, wrong email.'
+    return 1, 'No ERROR.'
+
+
+def customer_logout_check(request):
+    test_sessions = customer_sessions_check(request)
+    if test_sessions == False:
+        return 0, 'ERROR, session is broken.'
+    if customer_is_existent_by_email(request.session['ec_email']) == False:
+        return 0, 'ERROR, wrong email.'
+    return 1, 'No ERROR.'
+
+
+def communication_key_update_check(json_receive):
+    test_json = json_testing(json_receive, ['communication_key'], 1)
     if test_json == 1:
         return 0, 'ERROR, incomplete information.'
     if test_json == 2:
         return 0, 'ERROR, wrong information.'
-    if test_sessions == False:
-        return 0, 'ERROR, session is broken.'
-    if admin_is_existent_by_email(request.session['a_email']) == False:
-        return 0, 'ERROR, wrong email.'
+    if len(communication_key) != 32:
+        return 0, 'ERROR, communication key error.'
     return 1, 'No ERROR.'
-
-
-def admin_forget_password_email_request_check(json_receive):
-    test_json = json_testing(json_receive, ['email'], 1)
-    if test_json == 1:
-        return 0, 'ERROR, incomplete information.'
-    if test_json == 2:
-        return 0, 'ERROR, wrong information.'
-    if admin_is_existent_by_email(json_receive['email']) == False:
-        return 0, 'ERROR, wrong email.'
-    return 1, 'No ERROR.'
-
-
-# def admin_forget_password_check_vid_check(json_receive):
-#     test_json = json_testing(json_receive, ['email', 'vid'], 2)
-#     if test_json == 1:
-#         return 0, 'ERROR, incomplete information.'
-#     if test_json == 2:
-#         return 0, 'ERROR, wrong information.'
-#     if admin_is_existent_by_email_vid(json_receive['email'], json_receive['vid']) == False:
-#         return 0, 'ERROR, wrong email or vid.'
-#     return 1, 'No ERROR.'
-
-
-def admin_forget_password_save_data_check(json_receive):
-    test_json = json_testing(json_receive, ['email', 'newpassword'], 2)
-    if test_json == 1:
-        return 0, 'ERROR, incomplete information.'
-    if test_json == 2:
-        return 0, 'ERROR, wrong information.'
-    if admin_is_existent_by_email(json_receive['email']) == False:
-        return 0, 'ERROR, wrong email.'
-    return 1, 'No ERROR.'
-
-
-def admin_show_communication_key_check(request):
-    test_sessions = admin_sessions_check(request)
-    if test_sessions == False:
-        return 0, 'ERROR, session is broken.'
-    if admin_is_existent_by_email(request.session['a_email']) == False:
-        return 0, 'ERROR, wrong email.'
-    return 1, 'No ERROR.'
-
-
-def admin_reset_communication_key_check(request):
-    test_sessions = admin_sessions_check(request)
-    if test_sessions == False:
-        return 0, 'ERROR, session is broken.'
-    if admin_is_existent_by_email(request.session['a_email']) == False:
-        return 0, 'ERROR, wrong email.'
-    return 1, 'No ERROR.'
-
-
-def admin_show_cs_status_check(request):
-    test_sessions = admin_sessions_check(request)
-    if test_sessions == False:
-        return 0, 'ERROR, session is broken.'
-    if admin_is_existent_by_email(request.session['a_email']) == False:
-        return 0, 'ERROR, wrong email.'
-    return 1, 'No ERROR.'
-
-
-def admin_show_user_status_check(request):
-    test_sessions = admin_sessions_check(request)
-    if admin_is_existent_by_email(request.session['a_email']) == False:
-        return 0, 'ERROR, wrong email.'
-    if test_sessions == False:
-        return 0, 'ERROR, session is broken.'
-    return 1, 'No ERROR.'
-
-
-def admin_display_info_create_check(json_receive, request):
-    test_sessions = admin_sessions_check(request)
-    test_json = json_testing(json_receive, ['name', 'comment'], 2)
-    if test_sessions == False:
-        return 0, 'ERROR, session is broken.'
-    if admin_is_existent_by_email(request.session['a_email']) == False:
-        return 0, 'ERROR, wrong email.'
-    if displayinfo_is_existent_by_name(request.session['a_email'], json_receive['name']) == True:
-        return 0, 'ERROR, attribute name has been used.'
-    return 1, 'No ERROR'
-
-
-def admin_display_info_delete_check(json_receive, request):
-    test_sessions = admin_sessions_check(request)
-    test_json = json_testing(json_receive, ['name'], 1)
-    if test_sessions == False:
-        return 0, 'ERROR, session is broken.'
-    if admin_is_existent_by_email(request.session['a_email']) == False:
-        return 0, 'ERROR, wrong email.'
-    if displayinfo_is_existent_by_name(request.session['a_email'], json_receive['name']) == False:
-        return 0, 'ERROR, attribute is not existent.'
-    return 1, 'No ERROR'
-
-
-def admin_logout_check(request):
-    test_sessions = admin_sessions_check(request)
-    if test_sessions == False:
-        return 0, 'ERROR, session is broken.'
-    if admin_is_existent_by_email(request.session['a_email']) == False:
-        return 0, 'ERROR, wrong email.'
-    return 1, 'No ERROR.'
-
