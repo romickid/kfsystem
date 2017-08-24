b8681ccef9a228514194b00f25f4da631dc0197c
Archangel <xiaochen.gao@icloud.com>
2017-08-24 02:51:48 +0000

    Modify enterprise page. Ref #282

--- enterprise/frontend/src/pages/index/App.vue
+++ enterprise/frontend/src/pages/index/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <h2>
-        <a href="./en_login">跳转至用户登录页面</a>
-        <a href="./en_folders">跳转至用户注册页面</a>
-        <br>
-      </h2>
+    <div class='main-content'>
+      <div class='user-info'>{{ user_info }}</div>
+      <div class='div-button'><a href="./en_folders"><button class='button'>免费注册</button></a></div>
+      <div class='div-button'><a href="./en_login"><button class='button'>用户登录</button></a></div>
     </div>
   </div>
 </template>
 
 <script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      user_info: '未登录',
+      apiCustomerShowUserInfo: '../api/customer_show_user_info/'
+    }
+  },
+  methods: {
+    getUserInfoApi () {
+      this.$http.post(this.apiCustomerShowUserInfo)
+        .then((response) => {
+          if (response.data === 'ERROR, session is broken.') {
+            console.log('getUserInfoApi1')
+          } else if (response.data === 'ERROR, wrong email.') {
+            console.log('getUserInfoApi2')
+          } else {
+            this.user_info = response.data.email
+          }
+        })
+    }
+  },
+  created () {
+    this.getUserInfoApi()
+  }
+}
 </script>
 
 <style>
@@ -21,10 +44,41 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 body {
   overflow-y: scroll;
+  margin: 0;
+}
+
+.main-content {
+  background-image: url('./assets/background.png');
+  background-size: 100%;
+  height: 11780px;
+}
+
+.user-info {
+  width: 400px;
+  height: 31px;
+  background-color: #383d46;
+  border-bottom: 1px solid #50575f;
+  margin-left: 54.6em;
+  color: #fff;
+  padding-top: 10px;
+  text-align: right;
+}
+
+.div-button {
+  display: inline;
+  float: right;
+}
+
+.button {
+  color: #fff;
+  background-color: #1bc3d4;
+  border: 0;
+  font-size: 1em;
+  padding: 1.28em 2em;
+  border-left: 1px solid #383d46;
 }
 </style>
