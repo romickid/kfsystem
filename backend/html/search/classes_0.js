c3807c1a5c438c1ff83414a6cc0565f6c923c564
romickid <romic.kid@gmail.com>
2017-08-25 21:54:04 +0800

    Modify backend documents. Ref #307

--- backend/html/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['admin',['Admin',['../classbackend_1_1models_1_1_admin.html',1,'backend::models']]],
-  ['adminserializer',['AdminSerializer',['../classbackend_1_1serializers_1_1_admin_serializer.html',1,'backend::serializers']]]
-];
