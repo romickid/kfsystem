af12d179f9cb5ebabfc06429b5f8b6cfbd03a17f
romickid <romic.kid@gmail.com>
2017-08-25 13:18:25 +0000

    Done with documents. Ref #307

---
+++ backend/html/search/all_2.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['chattinglog',['ChattingLog',['../classbackend_1_1models_1_1_chatting_log.html',1,'backend::models']]],
+  ['chattinglogserializer',['ChattingLogSerializer',['../classbackend_1_1serializers_1_1_chatting_log_serializer.html',1,'backend::serializers']]],
+  ['customerservice',['CustomerService',['../classbackend_1_1models_1_1_customer_service.html',1,'backend::models']]],
+  ['customerservicecreateserializer',['CustomerServiceCreateSerializer',['../classbackend_1_1serializers_1_1_customer_service_create_serializer.html',1,'backend::serializers']]],
+  ['customerserviceserializer',['CustomerServiceSerializer',['../classbackend_1_1serializers_1_1_customer_service_serializer.html',1,'backend::serializers']]]
+];
